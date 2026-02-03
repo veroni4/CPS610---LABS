e2db567ba132da0ccdeaffe4e3562d41ab49c576
Veronica Kastulin <vero.kastulin@gmail.com>
2026-02-03 14:02:46 -0500

    final version

--- calGPA.java
+++ calGPA.java
@@ -6,42 +6,45 @@ import java.util.Scanner;
 class calGPA{
     public static void main (String args[]) throws SQLException, IOException{
         try{
+            // Load Oracle JDBC Driver
             Class.forName("oracle.jdbc.driver.OracleDriver");
         }catch (ClassNotFoundException x){
             System.out.println("Driver could not be loaded.");
         }
+        // Creating scanner object to read input
         Scanner scanner = new Scanner(System.in);
-        String dbacct, passwrd, name;
+        String dbacct, passwrd, name; // Initialize variables for db connection
         char grade;
         int credit;
-        // use scanner to record user input
+        //use scanner to record user input
         System.out.println("Enter database account: "); 
-        dbacct = scanner.next();
+        dbacct = scanner.nextLine();
         System.out.println("Enter password: ");
-        passwrd = scanner.next();
+        passwrd = scanner.nextLine();
+        // Create a connection to oracle db using user input username and password
         Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@orsrv1.cs.torontomu.ca:1521/orcldb", dbacct, passwrd);
-        String stmt1 = "select G.Grade, C.Credit_hours from STUDENT S, GRADE_REPORT G, SECTION SEC, COURSE C where G.Student_number=S.Student_number AND G.Section_identifier=SEC.Section_identifier AND SEC.Course_number=C.Course_number AND S.Name=?";
-        PreparedStatement p = conn.prepareStatement(stmt1);
-        System.out.println("Please enter your name: ");
-        name = scanner.next();
+        String stmt1 = "select G.Grade, C.Credit_hours from STUDENT S, GRADE_REPORT G, SECTION SEC, COURSE C where G.Student_number=S.Student_number AND G.Section_identifier=SEC.Section_identifier AND SEC.Course_number=C.Course_number AND TRIM(S.Name)=?";
+        PreparedStatement p = conn.prepareStatement(stmt1); // Create a prepared statement p from the sql command
+        System.out.println("Please enter your name: "); // Ask user for student name for whom to calculate GPA
+        name = scanner.nextLine();
         scanner.close();
-        p.clearParameters();
-        p.setString(1, name);
-        ResultSet r = p.executeQuery();
+        p.clearParameters(); // clear all previous parameters on the prepared statement
+        p.setString(1, name); // sets pos1 paramerter to value of name, replacing ? in sql command
+        ResultSet r = p.executeQuery(); // execute query and store result in ResultSet r
         double count=0, sum=0, avg=0;
         while(r.next()){
-            grade = r.getString(1).charAt(0);
+            grade = r.getString(1).charAt(0); // extract grade and credit hours from result set
             credit = r.getInt(2);
-        switch (grade){
-            case 'A': sum=sum+(4*credit); count=count+1; break;
-            case 'B': sum=sum+(3*credit); count=count+1; break;
-            case 'C': sum=sum+(2*credit); count=count+1; break;
-            case 'D': sum=sum+(1*credit); count=count+1; break;
-            case 'F': sum=sum+(0*credit); count=count+1; break;
-            default: System.out.println("This grade "+grade+" will not be calculated."); break;
-        }
+            switch (grade){ // switch case to calculate grade points based on grade received
+                case 'A': sum=sum+(4*credit); count=credit+1; break;
+                case 'B': sum=sum+(3*credit); count=credit+1; break;
+                case 'C': sum=sum+(2*credit); count=credit+1; break;
+                case 'D': sum=sum+(1*credit); count=credit+1; break;
+                case 'F': sum=sum+(0*credit); count=credit+1; break;
+                default: System.out.println("This grade "+grade+" will not be calculated."); break;
+            }
         };
-        avg = sum/count;
+        avg = sum/count; // calculate GPA
         System.out.println("Student named "+name+" has a grade point average "+avg+".");
         r.close();
     }
